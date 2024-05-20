30fbf8a04a7f08244c5c586111527f865a2fea65
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-20 16:33:33 +0200

    Car Models Update

--- car_dealership/cars/models.py
+++ car_dealership/cars/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    established_date = models.DateField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    PETROL_CHOICES = [
+        ('Petrol', 'Petrol'),
+        ('Diesel', 'Diesel'),
+        ('Electric', 'Electric'),
+        ('Hybrid', 'Hybrid'),
+    ]
+
+    CAR_TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('Truck', 'Truck'),
+        ('Coupe', 'Coupe'),
+        ('Convertible', 'Convertible'),
+        ('Hatchback', 'Hatchback'),
+        ('Van', 'Van'),
+        ('Wagon', 'Wagon'),
+    ]
+
+    GEAR_CHOICES = [
+        ('Manual', 'Manual'),
+        ('Automatic', 'Automatic'),
+    ]
+
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    model_name = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='car_images/', blank=True, null=True)
+    petrol_type = models.CharField(
+        max_length=10, choices=PETROL_CHOICES, default='Petrol')
+    car_type = models.CharField(
+        max_length=12, choices=CAR_TYPE_CHOICES, default='Sedan')
+    gear_type = models.CharField(
+        max_length=10, choices=GEAR_CHOICES, default='Manual')
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.model_name} ({self.year})"
+
+
