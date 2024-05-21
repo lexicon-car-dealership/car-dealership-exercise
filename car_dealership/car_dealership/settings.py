dd4c3a0e37c450c3823f432eb36c23e8e899c4a5
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-21 14:10:35 +0200

    Add forms, urls and settings for auth

--- car_dealership/car_dealership/settings.py
+++ car_dealership/car_dealership/settings.py
@@ -133,6 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication Redirects
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- car_dealership/car_dealership/urls.py
+++ car_dealership/car_dealership/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 
 from .views import index
 
@@ -24,6 +24,7 @@ from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('auth/', include('authentication.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
