143be5ddc76da5f2e14a28b46027e4c05ff0a3e5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-23 15:18:57 +0200

    Add get car by id and get most recent paginated

--- car_dealership/cars/views.py
+++ car_dealership/cars/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from . import models
 
 # Create your views here.
+
+
+def get_car_by_id(request, car_id):
+    car = get_object_or_404(models.Car, pk=car_id)
+    return HttpResponse(car)
+
+
+def get_most_recent_paginated(request):
+    cars = models.Car.objects.all()
+    cars.order_by('created_at')
+    paginator = Paginator(cars, 10)
+    page = request.GET.get('page', 1)
+    try:
+        cars_page = paginator.page(page)
+    except PageNotAnInteger:
+        cars_page = paginator.page(1)
+    except EmptyPage:
+        cars_page = paginator.page(paginator.num_pages)
+
+    return HttpResponse(cars_page)
