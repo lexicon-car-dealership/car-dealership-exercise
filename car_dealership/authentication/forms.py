6ce7bcc8a03ac7f93a8753393ce0806d256f8d4b
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-23 22:36:06 +0200

    Introduces EditProfileForm with logic to hide staff-related fields for non-staff users.

--- car_dealership/authentication/forms.py
+++ car_dealership/authentication/forms.py
@@ -9,3 +9,29 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'is_staff',
+                  'is_superuser', 'groups', 'user_permissions']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'is_staff': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'is_superuser': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'groups': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'user_permissions': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+        
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        # If the user is not a staff member, hide the staff and superuser fields
+        if user and not user.is_staff:
+            self.fields['is_staff'].widget = forms.HiddenInput()
+            self.fields['is_superuser'].widget = forms.HiddenInput()
+            self.fields['groups'].widget = forms.HiddenInput()
+            self.fields['user_permissions'].widget = forms.HiddenInput()
