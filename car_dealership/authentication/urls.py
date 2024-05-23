1bd915be58045f20ad4fc5a8fcc48d26ba63c3f9
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-23 22:36:54 +0200

    Add profile view and URL routes

--- car_dealership/authentication/urls.py
+++ car_dealership/authentication/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
+    path('profile/<int:profile_id>/', views.profile_view, name='profile'),
+    path('profile/', views.profile_view, name='profile'),
 ]
