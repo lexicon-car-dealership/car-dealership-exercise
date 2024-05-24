254bfd4ca1427a24e289ce9ff5c88c78c71cf707
Sergei Pavliuk <climbcoder@gmail.com>
2024-05-24 15:41:16 +0200

    beta

--- car_dealership/faker/__init__.py
+++
@@ -1,7 +0,0 @@
-from faker import Faker
-from vehicle_faker import VehicleProvider
-fake = Faker()
-fake.add_provider(VehicleProvider)
-
-print(fake.vehicle_generated_vin())
-print(fake.vehicle_year_make_model_cat_vin())
