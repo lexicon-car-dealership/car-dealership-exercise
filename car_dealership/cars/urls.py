6de5f5d6ccfbefa6d2aef57d58347306db83b3a2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-24 14:12:26 +0200

    rename view to index

--- car_dealership/cars/urls.py
+++ car_dealership/cars/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_most_recent_paginated, name='cars'),
+    path('', views.get_most_recent_paginated, name='index'),
     path('<int:car_id>/', views.get_car_by_id, name='car'),
 ]
