a592d164351806d1a08920e685d676f084b2b1a3
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-27 15:10:24 +0200

    Add admin dynamic url for form

--- car_dealership/cars/urls.py
+++ car_dealership/cars/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.get_most_recent_paginated, name='index'),
     path('<int:car_id>/', views.get_car_by_id, name='car'),
+    path('form/<str:form_type>/', views.admin_forms, name='admin_forms'),
+
 ]
