d70ef5dca473f126e875202de7d5c8dfb4c26fed
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-27 15:22:03 +0200

    Add admin_form view

--- car_dealership/cars/views.py
+++ car_dealership/cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.contrib import messages
+from .forms import ManufacturerForm, BrandModelForm, CarForm, CarImagesForm
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from . import models
 
@@ -9,30 +10,33 @@ def index(request):
 
 
 def get_car_by_id(request, car_id):
-    car = get_object_or_404(models.Car, pk=car_id)
-    return HttpResponse(car)
+    car = get_object_or_404(
+        models.Car.objects.prefetch_related('carimages_set'), id=car_id)
+    car_images = car.carimages_set.all()
+    return render(request, 'car_detail.html', {'car': car, 'car_images': car_images})
+
 
 
 def get_most_recent_paginated(request):
-    brandFilter = request.GET.get('brand', None)
-    modelFilter = request.GET.get('model', None)
-    fuelFilter = request.GET.get('fuel', None)
-    yearFilter = request.GET.get('year', None)
-    minPriceFilter = request.GET.get('minPrice', None)
-    maxPriceFilter = request.GET.get('maxPrice', None)
+    brand_filter = request.GET.get('brand', None)
+    model_filter = request.GET.get('model', None)
+    fuel_filter = request.GET.get('fuel', None)
+    year_filter = request.GET.get('year', None)
+    min_price_filter = request.GET.get('minPrice', None)
+    max_price_filter = request.GET.get('maxPrice', None)
     filters = {}
-    if brandFilter:
-        filters['model_name__manufacturer__name__iexact'] = brandFilter
-    if modelFilter:
-        filters['model_name__name__iexact'] = modelFilter
-    if fuelFilter:
-        filters['petrol_type'] = fuelFilter
-    if yearFilter:
-        filters['year'] = yearFilter
-    if minPriceFilter:
-        filters['price__gte'] = minPriceFilter
-    if maxPriceFilter:
-        filters['price__lte'] = maxPriceFilter
+    if brand_filter:
+        filters['model_name__manufacturer__name__iexact'] = brand_filter
+    if model_filter:
+        filters['model_name__name__iexact'] = model_filter
+    if fuel_filter:
+        filters['petrol_type'] = fuel_filter
+    if year_filter:
+        filters['year'] = year_filter
+    if min_price_filter:
+        filters['price__gte'] = min_price_filter
+    if max_price_filter:
+        filters['price__lte'] = max_price_filter
     cars = models.Car.objects.filter(**filters).order_by('created_at')
     paginator = Paginator(cars, 3)
     page = request.GET.get('page', 1)
@@ -45,3 +49,43 @@ def get_most_recent_paginated(request):
 
     params = request.GET.copy()
     return render(request, 'index.html', {'cars': cars_page, 'page_obj': cars_page, 'params': params})
+
+
+def admin_forms(request, form_type):
+    form_map = {
+        'manufacturer': ManufacturerForm,
+        'brandmodel': BrandModelForm,
+        'car': CarForm,
+        'carimages': CarImagesForm,
+    }
+
+    if request.method == 'POST':
+        if form_type == 'carimages':
+            form = CarImagesForm(request.POST, request.FILES)
+            if form.is_valid():
+                car = form.cleaned_data['car']
+                images = form.cleaned_data['image']
+                for image in images:
+                    models.CarImages.objects.create(car=car, image=image)
+                messages.success(
+                    request, 'Car images uploaded successfully.')
+                return redirect('index')
+            else:
+                print(form.errors)
+                messages.error(
+                    request, 'Form is not valid. Please check the fields.')
+        else:
+            form = form_map[form_type](request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(
+                    request, f'{form_type.capitalize()} saved successfully.')
+                return redirect('index')
+            else:
+                print(form.errors)
+                messages.error(
+                    request, 'Form is not valid. Please check the fields.')
+    else:
+        form = form_map[form_type]()
+        
+    return render(request, 'forms/admin.html', {'form': form})
