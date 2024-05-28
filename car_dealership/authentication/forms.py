ec294fda27f60888774fe2d62985a5626e9f733d
Ayoub Afi <59549940+afiay@users.noreply.github.com>
2024-05-28 10:23:06 +0200

    Fixed issue with the hidden fields #6

--- car_dealership/authentication/forms.py
+++ car_dealership/authentication/forms.py
@@ -32,14 +32,18 @@ class EditProfileForm(forms.ModelForm):
             'groups': forms.SelectMultiple(attrs={'class': 'form-control'}),
             'user_permissions': forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
-        
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists")
+        return email
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         super(EditProfileForm, self).__init__(*args, **kwargs)
-        # If the user is not a staff member, hide the staff and superuser fields
         if user and not user.is_staff:
-            self.fields['is_staff'].widget = forms.HiddenInput()
-            self.fields['is_superuser'].widget = forms.HiddenInput()
-            self.fields['groups'].widget = forms.HiddenInput()
-            self.fields['user_permissions'].widget = forms.HiddenInput()
+            del self.fields['is_staff']
+            del self.fields['is_superuser']
+            del self.fields['groups']
+            del self.fields['user_permissions']
