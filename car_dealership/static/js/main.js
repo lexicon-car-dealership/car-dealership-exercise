3ff3408ffc4f46b4ab72dfd5b78dc71341fdca92
kortik425 <jaycee425.develop@gmail.com>
2024-05-29 12:15:34 +0200

    Add filters toggle functionality

--- car_dealership/static/js/main.js
+++ car_dealership/static/js/main.js
@@ -11,9 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
   //     window.location.href = `${filterForm.action}?${urlParams.toString()}`;
   //   }
   // });
+  //profile stuff
   const profileDropdown = document.getElementById('navbarDropdown');
   const menuBar = document.getElementById('menu-bar');
   profileDropdown.addEventListener('click', () => {
     menuBar.classList.toggle('active');
+  })
+  //filters
+  const filterButton = document.getElementById('filter-toggle');
+  const navSearchFilters = document.getElementById('nav-search-filters');
+  filterButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    navSearchFilters.classList.toggle('active');
   })
 });
