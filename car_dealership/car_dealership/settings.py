47998dded2ac04ced528d0a4ffbf7ae6572c354e
Sergei Pavliuk <climbcoder@gmail.com>
2024-05-30 13:41:01 +0200

    final (not really, will optimize)

--- car_dealership/car_dealership/settings.py
+++ car_dealership/car_dealership/settings.py
@@ -70,6 +70,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cars',
     'authentication',
+    #'seed_faker',
+    #'seed',
 ]
 
 MIDDLEWARE = [
--- car_dealership/vehicle_dict.py
+++ car_dealership/vehicle_dict.py
@@ -1,5 +1,4 @@
-#from pathlib import Path
-#print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
+
 vehicles = [
   {
     "year": 2020,
